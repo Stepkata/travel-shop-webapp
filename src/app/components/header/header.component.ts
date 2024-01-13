3ad0b14afa8f79b8ea98a6f29e5cd09eda545881
katstep <otiofthebag@protonmail.com>
2024-01-13 01:59:32 +0100

    notification

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,11 +12,24 @@ export class HeaderComponent implements OnInit{
   cart: Cart = new Cart();
   isSmallScreen = false;
   isLoggedIn: boolean = false;
+  history: HistoryItem[] = [];
+
+  oncoming:boolean = false;
+  isLoading: boolean = true;
+  comingStart: string = "";
+  comingName: string = "";
 
   constructor( private DataService: DataService, private AccountService: AccountService) { 
   }
 
   ngOnInit(): void {
+    this.DataService.history$.subscribe((data) => {
+      if (data == null)
+        console.log("null!");
+      this.history = data;
+      this.checkTripNotif();
+      this.isLoading = false;
+    });
       this.DataService.cart$.subscribe((data) => {
         if (data != null){
           this.cart = data;
@@ -34,5 +47,26 @@ export class HeaderComponent implements OnInit{
     // Update the isSmallScreen property based on the window width
     this.isSmallScreen = window.innerWidth <= 768; // Adjust the breakpoint as needed
   }
+
+  checkTripNotif(){
+    for (const item of this.history){
+      const startDate = new Date(item.startDate);
+      const currentDate = new Date();
+      const timeDifference = startDate.getTime() - currentDate.getTime();
+
+      // Convert milliseconds to days
+      const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+      // Check if the difference is less than 4 days
+      if (daysDifference > 0 && daysDifference < 4){
+        this.oncoming = true;
+        this.comingName = item.Name;
+        this.comingStart = item.startDate;
+        return;
+      }
+    }
+    this.oncoming = false;
+  }
+
 
 }
