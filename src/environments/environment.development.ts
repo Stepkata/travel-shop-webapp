24e729b5062eedfa2dc525e7fe8c0845d9757ea1
katstep <otiofthebag@protonmail.com>
2024-03-06 22:27:03 +0100

    added video presentation

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,18 @@
 export const environment = {
     firebaseConfig: {
-        apiKey: "AIzaSyB4QQv5Y-AMPRlEGBArhntSIVdZv_TGJjc",
-        authDomain: "wycieczki-angular.firebaseapp.com",
-        projectId: "wycieczki-angular",
-        storageBucket: "wycieczki-angular.appspot.com",
-        messagingSenderId: "556832596255",
-        appId: "1:556832596255:web:38824527d2e47120bb102a"
+         apiKey: "AIzaSyB4QQv5Y-AMPRlEGBArhntSIVdZv_TGJjc",
+
+  authDomain: "wycieczki-angular.firebaseapp.com",
+
+  databaseURL: "https://wycieczki-angular-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "wycieczki-angular",
+
+  storageBucket: "wycieczki-angular.appspot.com",
+
+  messagingSenderId: "556832596255",
+
+  appId: "1:556832596255:web:f2d92da78d54eb4fbb102a"
+
       }
 };
